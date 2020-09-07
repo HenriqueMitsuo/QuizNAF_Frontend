0cdcfad51cf387200b5ce6c81368212b220cf831
Jonas Lima <jonaslima015@gmail.com>
2020-09-07 11:05:16 -0300

    protótipo de NavBar adicionado ao App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,27 @@
 <template>
+<div class="app">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="navbar-brand">NavBar</div>
+    <span class="navbar-toggler-icon"></span>
+    <div class="collapse navbar-collapse" id="navbarToggle">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                <div class="nav-link">
+                  <router-link to="/">Home</router-link>
+                </div>
+              </li>
+              <li class="nav-item">
+                <div class="nav-link">
+                  <router-link to="/about">About</router-link>
+                </div>
+              </li>
+          </ul>
+      </div>
+  </nav>
+  <router-view/>
+</div>
+
+<!--
   <div id="app">
     <nav class="text-center">
       <router-link to="/">Home</router-link> |
@@ -6,6 +29,8 @@
     </nav>
     <router-view/>
   </div>
+-->
+
 </template>
 
 <style>
