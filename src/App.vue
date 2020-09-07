aeae93ae49ddede62eda5f448e5f4d8545a7b669
Jonas Lima <jonaslima015@gmail.com>
2020-09-07 19:00:49 -0300

    Alterado navbar para componente

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,5 @@
 <template>
   <div class="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-blue-container">
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarToggle"
-        aria-controls="navbarToggleExternalContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <span class="navbar-brand">App Title</span>
-
-      <div class="collapse navbar-collapse" id="navbarToggle">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <div class="nav-link">
-              <router-link to="/">Home</router-link>
-            </div>
-          </li>
-          <li class="nav-item">
-            <div class="nav-link">
-              <router-link to="/about">About</router-link>
-            </div>
-          </li>
-          <li class="nav-item">
-            <div class="nav-link">
-              <router-link to="/Login">Login</router-link>
-            </div>
-          </li>
-          <li class="nav-item">
-            <div class="nav-link">
-              <router-link to="/Question">Question</router-link>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
     <router-view />
   </div>
 
