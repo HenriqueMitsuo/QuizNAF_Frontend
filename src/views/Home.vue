662538f3f2b2ae08660684fd4d35aae069d99688
Jonas Lima <jonaslima015@gmail.com>
2020-09-07 13:27:39 -0300

    Criado e realocado para componentes na Home.Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,120 +4,22 @@
     <div class="bg-blue px-3 pb-4"> <!-- px define padding left e right -->
       <div class="container">
 
-        <!-- Linha do card principal expandido -->
+        <!-- Linha do Tópico principal expandido -->
+        <TopicExpanded />
+        
+        <!-- Linha para com dois Tópicos | pt-4 coloca um espaçamento do componente acima -->
         <div class="row pt-4">
-
-          <div class="col-12">
-            <div class="card bg-blue-container text-white">
-              <div class="card-body">
-                <h5 class="card-title">Tópico destaque</h5>
-                <!-- img -->
-                <p class="card-text text-light">Descrição</p>
-                 
-              
-                <div class="progress-custom">
-                  <div class="progress" style="height: 10px">
-                    <div class="progress-bar bg-danger" role="progressbar" style="width: 8%" aria-valuenow="8" aria-valuemin="0" aria-valuemax="100">
-                    </div>  
-                  </div>
-                  <div class="progress-value">
-                    1/12
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
+        
+          <Topic />
+          <Topic />
 
         </div>
 
-        <!-- Linha com dois cards -->
+        <!-- Quebra de linha -->
         <div class="row pt-4">
-
-          <div class="col-6 pr-n3">
-            <div class="card bg-blue-container text-white">
-              <img class="card-img-top rounded" src="@/assets/bg-prototype.png">
-              <div class="card-body">
-                <p class="card-text text-light">Sociologia</p>
-                
-                <div class="progress-custom mt-n2">
-                  <div class="progress" style="height: 10px">
-                    <div class="progress-bar bg-warning" role="progressbar" style="width: 33%" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100">
-                    </div>  
-                  </div>
-                  <div class="progress-value">
-                    5/15
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
-
-          <div class="col-6">
-            <div class="card bg-blue-container text-white">
-              <img class="card-img-top rounded" src="@/assets/bg-prototype.png">
-              <div class="card-body">
-                <p class="card-text text-light">Direito penal</p>
-                
-                <div class="progress-custom mt-n2">
-                  <div class="progress" style="height: 10px">
-                    <div class="progress-bar bg-warning" role="progressbar" style="width: 33%" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100">
-                    </div>  
-                  </div>
-                  <div class="progress-value">
-                    5/15
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-        <!-- Linha com outros 2 cards -->
-        <div class="row pt-4">
-
-          <div class="col-6">
-            <div class="card bg-blue-container text-white">
-              <img class="card-img-top rounded" src="@/assets/bg-prototype.png">
-              <div class="card-body">
-                <p class="card-text text-light">Logistica</p>
-                
-                <div class="progress-custom mt-n2">
-                  <div class="progress" style="height: 10px">
-                    <div class="progress-bar bg-warning" role="progressbar" style="width: 33%" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100">
-                    </div>  
-                  </div>
-                  <div class="progress-value">
-                    5/15
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
-
-          <div class="col-6">
-            <div class="card bg-blue-container text-white">
-              <img class="card-img-top rounded" src="@/assets/bg-prototype.png">
-              <div class="card-body">
-                <p class="card-text text-light">Direito</p>
-                
-                <div class="progress-custom mt-n2">
-                  <div class="progress" style="height: 10px">
-                    <div class="progress-bar bg-warning" role="progressbar" style="width: 33%" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100">
-                    </div>  
-                  </div>
-                  <div class="progress-value">
-                    5/15
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
+          
+          <Topic />
+          <Topic />
 
         </div>
 
@@ -127,23 +29,37 @@
   </div>
 </template>
 
+<script>
+import TopicExpanded from "@/components/TopicExpanded.vue";
+import Topic from "@/components/Topic.vue";
+
+export default {
+  components: {
+    TopicExpanded,
+    Topic
+  }
+};
+</script>
+
 <style>
+/* Estilização para deixar os numeros ao da progress bar */ 
 .progress-custom {
-        display: table;
-        width: 100%;
-    }
+  display: table;
+  width: 100%;
+}
 
-    .progress-custom .progress{
-        margin-bottom: 7px;
-        vertical-align: middle;
-    }
+.progress-custom .progress{
+  margin-bottom: 7px;
+  vertical-align: middle;
+}
 
-    .progress-custom .progress-value{
-        display: table-cell;
-        width: 1%; 
-        padding: 0 4px; /*optionally*/
-    }
+.progress-custom .progress-value{
+  display: table-cell;
+  width: 1%; 
+  padding: 0 4px; /*optionally*/
+}
 </style>
+
 
 <!--
 <template>
