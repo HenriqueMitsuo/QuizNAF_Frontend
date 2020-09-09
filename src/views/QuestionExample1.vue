b8d442ad2d6ee88383b9fde2f3683c3f37bd1a57
MoisesDuarte <moisesduartejunior@gmail.com>
2020-09-09 18:02:31 -0300

    Correções em views

--- src/views/QuestionExample1.vue
+++ src/views/QuestionExample1.vue
@@ -5,10 +5,11 @@
 
     <div class="bg-blue">
       <div class="container">
+
         <div class="row">
           <div class="col-12 py-5">
             <p class="text-light text-center" style="font-size: 1.2rem">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin nisi in ultricies accumsan. Donec pretium rhoncus sollicitudin. Mauris pretium, urna eu ornare dictum
+              {{ questionText }}
             </p>
           </div>
         </div>
@@ -36,6 +37,7 @@ export default {
   },
   data() {
     return {
+      questionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin nisi in ultricies accumsan. Donec pretium rhoncus sollicitudin. Mauris pretium, urna eu ornare dictum',
       questions: [
         { id: 1, text: 'Lorem Ipsum Ent Dolor' },
         { id: 2, text: 'Sit amet requiescat in pace' },
--- src/views/QuestionExample2.vue
+++ src/views/QuestionExample2.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="app">
     <QuestionBar />
-    <!-- <QuestionStatusBar /> -->
 
     <div class="bg-blue">
       <div class="container">
-        <!-- Texto da questão -->
+
         <div class="row">
           <div class="col-12 py-5">
             <p class="text-light text-center" style="font-size: 1.2rem">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin nisi in ultricies accumsan. Donec pretium rhoncus sollicitudin. Mauris pretium, urna eu ornare dictum
+              {{ questionText }}
             </p>
           </div>
         </div>
 
-        <QuestionAlternative />
+        <QuestionAlternative :questions="questions" />
 
-        <!-- Botão fora do wireframe -->
         <div class="py-2 text-center">
           <button class="btn btn-success p2">Conferir</button>
         </div>
@@ -37,6 +35,7 @@ export default {
   },
   data() {
     return {
+      questionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin nisi in ultricies accumsan. Donec pretium rhoncus sollicitudin. Mauris pretium, urna eu ornare dictum',
       questions: [
         { id: 1, text: 'Lorem Ipsum Ent Dolor' },
         { id: 2, text: 'Sit amet requiescat in pace' },
