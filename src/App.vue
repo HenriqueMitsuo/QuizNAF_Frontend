182e4e2f882ec3c9b52374579278c83e9f9301b6
HenriqueMitsuo <henrique.informatica123@gmail.com>
2020-09-10 21:14:38 -0300

    Adicionado exemplo para utilização do ApiService

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,23 @@
   </div>
 </template>
 
+<script>
+import { ApiService } from "@/services/ApiService";
+
+export default {
+  data() {
+    return {
+      apiService: new ApiService('quiz'),
+      quiz: []
+    }
+  },
+  async mounted() {
+    this.quiz = await this.apiService.queryAll();
+    console.log(this.quiz);
+  }
+};
+</script>
+
 <style>
 /* Cores do container */
 body {
