ad51e03dcf8c236446efde8cbd54b5517a75bb6d
MoisesDuarte <moisesduartejunior@gmail.com>
2020-09-14 17:38:27 -0300

    Limpando estilo em App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,30 +7,9 @@
 </template>
 
 <script>
-// import { ApiService as QuizService } from "@/services/ApiService";
 
-// export default {
-//   data() {
-//     return {
-//       quizService: new QuizService('quiz'),
-//       quiz: []
-//     }
-//   },
-//   async mounted() {
-//     this.quiz = await this.quizService.queryAll();
-//     console.log(this.quiz);
-//   }
-// };
 </script>
 
 <style>
-/* Cores do container */
-body {
-  background-color: #2b3c4d !important;
-}
 
-/* Cor do plano de fundo */
-.bg-blue-container {
-  background-color: #34495e !important;
-}
 </style>
