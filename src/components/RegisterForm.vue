def5dfeeb66a1daead329fcb59d0e96014dac779
Jonas Lima <jonaslima015@gmail.com>
2020-09-14 13:51:13 -0300

    parte logica para cadastro do usuario

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="container">
-    <form action method class="mt-4">  
-
+    <form @submit.prevent="RegisterUser" method="post" class="mt-4">
       <div class="input-group input-group-lg mb-3">
         <div class="input-group-prepend">
           <span class="input-group-text">
             <i class="fas fa-user-circle"></i>
           </span>
         </div>
-        <input type="text" name class="form-control text-light" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Nome"/>
+        <input
+          type="text"
+          v-model="User.name"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Nome"
+          required
+        />
       </div>
-      
+
       <div class="input-group input-group-lg mb-3">
         <div class="input-group-prepend">
           <span class="input-group-text" style="background: transparent;">
             <i class="fas fa-envelope"></i>
           </span>
         </div>
-        <input type="text" name class="form-control text-light" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="E-mail"/>
-      </div>     
+        <input
+          type="text"
+          v-model="User.email"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="E-mail"
+          required
+        />
+      </div>
 
       <div class="input-group input-group-lg mb-3">
         <div class="input-group-prepend">
@@ -26,9 +41,16 @@
             <i class="fas fa-globe"></i>
           </span>
         </div>
-        <input type="text" name class="form-control text-light" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="País" />
+        <input
+          type="text"
+          v-model="User.country"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="País"
+          required
+        />
       </div>
-
 
       <div class="input-group input-group-lg mb-3">
         <div class="input-group-prepend">
@@ -36,10 +58,16 @@
             <i class="fas fa-building"></i>
           </span>
         </div>
-        <input type="text" name class="form-control text-light" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Cidade" />
-      </div>    
-          
-   
+        <input
+          type="text"
+          v-model="User.city"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Cidade"
+          required
+        />
+      </div>
 
       <div class="input-group input-group-lg mb-3">
         <div class="input-group-prepend">
@@ -47,7 +75,15 @@
             <i class="fas fa-school"></i>
           </span>
         </div>
-        <input type="text" name class="form-control text-light" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Instituição de ensino" />
+        <input
+          type="text"
+          v-model="User.educationInstitute"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Instituição de ensino"
+          required
+        />
       </div>
 
       <div class="input-group input-group-lg mb-3">
@@ -56,7 +92,15 @@
             <i class="fa fa-pen"></i>
           </span>
         </div>
-        <input type="text" name class="form-control text-light" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Curso" />
+        <input
+          type="text"
+          v-model="User.educationCourse"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Curso"
+          required
+        />
       </div>
 
       <div class="input-group input-group-lg mb-3">
@@ -65,7 +109,15 @@
             <i class="fas fa-lock"></i>
           </span>
         </div>
-        <input type="password" name class="form-control text-light" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Senha" />
+        <input
+          type="password"
+          v-model="User.password"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Senha"
+          required
+        />
       </div>
 
       <div class="input-group input-group-lg">
@@ -74,22 +126,46 @@
             <i class="fas fa-lock"></i>
           </span>
         </div>
-        <input type="password" name class="form-control text-light" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="Repita a senha" />
+        <input
+          type="password"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Repita a senha"
+          required
+        />
       </div>
 
-      <router-link tag="button" class="btn btn-warning btn-block mt-3" to="/">
-        REGISTRAR
-      </router-link>
+      <button type="submit" class="btn btn-warning btn-block mt-3">REGISTRAR</button>
     </form>
   </div>
 </template>
 
 <script>
+import { ApiService as UserService } from "@/services/ApiService";
+
 export default {
-  
-}
+  data() {
+    return {
+      userService: new UserService("register"),
+      User: {
+        name: null,
+        email: null,
+        country: null,
+        city: null,
+        educationInstitute: null,
+        educationCourse: null,
+        password: null,
+      },
+    };
+  },
+  methods: {
+    RegisterUser: async function () {
+      await this.userService.createUser(this.User);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
