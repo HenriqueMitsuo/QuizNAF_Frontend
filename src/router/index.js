5fb4076de02a43ef3eca1d799c1b01bc7ec9d618
MoisesDuarte <moisesduartejunior@gmail.com>
2020-09-15 20:33:18 -0300

    Organizando Route Guards em index.js

--- src/router/index.js
+++ src/router/index.js
@@ -20,30 +20,34 @@ const routes = [
     path: "/Home",
     name: "Home",
     component: () => import("../views/Home.vue"),
-    beforeEnter (_, __, next) {
-      checkAuth() ? next() : next('/')
-    }
+    meta: { requiresAuth: true }
   },
   {
     path: "/Question/:id",
     name: "Question",
     component: () => import("../views/Question.vue"),
-    beforeEnter (_, __, next) {
-      checkAuth() ? next() : next('/')
-    }
+    meta: { requiresAuth: true }
   },
   {
     path: "/Profile",
     name: "Profile",
     component: () => import("../views/Profile.vue"),
-    beforeEnter (_, __, next) {
-      checkAuth() ? next() : next('/')
-    }
+    meta: { requiresAuth: true }
   }
 ];
 
 const router = new VueRouter({
   routes
+});
+
+// Route Guards
+router.beforeEach((to, from, next) => {
+  if ((to.matched.some(record => record.meta.requiresAuth))) {
+    // Essa rota requer auth. Se não estiver logado, redirecionar para tela de login
+    checkAuth() ? next() : next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
