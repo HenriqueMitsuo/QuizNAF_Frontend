42a49a06ee2c82364b3dbe76ba82c81ba0254d17
HenriqueMitsuo <henrique.informatica123@gmail.com>
2020-09-22 10:54:55 -0300

    Refatorando async await do ApiService

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -3,47 +3,51 @@ import { axios } from "@/plugins/axios";
 export class ApiService {
   constructor(Resource) {
     this.Resource = Resource;
-    this.Token = { 'Authorization': `Bearer ${localStorage.getItem('token')}` };
+    this.Token = { Authorization: `Bearer ${localStorage.getItem("token")}` };
   }
 
   async queryAll() {
-    return await axios.get(this.Resource, { headers: this.Token })
-      .then(response => response.data);
+    try {
+      const response = await axios.get(this.Resource, { headers: this.Token });
+      return await response.data;
+    } catch (error) {
+      console.log(`Server error: ${error}`)
+    }
   }
 
   async queryOne(id) {
-    return await axios.get(`${this.Resource}/${id}`, { headers: this.Token })
-      .then(response => response.data);
+    const response = await axios.get(`${this.Resource}/${id}`, { headers: this.Token });
+    return await response.data;
   }
 
   async queryFilter(filter) {
-    return await axios.get(this.Resource, { headers: this.Token ,params: filter })
-      .then(response => response.data);
+    const response = await axios.get(this.Resource, { headers: this.Token, params: filter });
+    return await response.data;
   }
 
   async createOne(data) {
-    return await axios.post(this.Resource, data, { headers: this.Token })
-      .then(response => response.data);
+    const response = await axios.post(this.Resource, data, { headers: this.Token });
+    return await response.data;
   }
 
   async updateOne(id, data) {
-    return await axios.put(`${this.Resource}/${id}`, data, { headers: this.Token })
-      .then(response => response.data);
+    const response = await axios.put(`${this.Resource}/${id}`, data, { headers: this.Token });
+    return await response.data;
   }
 
   async deleteOne(id) {
-    return await axios.delete(`${this.Resource}/${id}`, { headers: this.Token })
-      .then(response => response.data);
+    const response = await axios.delete(`${this.Resource}/${id}`, { headers: this.Token });
+    return await response.data;
   }
 
   // Controle de usuário (Auth de login, Criação)
   async login(data) {
-    return await axios.post(this.Resource, data)
-      .then(response => response.data);
+    const response = await axios.post(this.Resource, data);
+    return await response.data;
   }
 
   async createUser(data) {
-    return await axios.post('register', data)
-      .then(response => response.data);
+    const response = await axios.post("register", data);
+    return await response.data;
   }
 }
