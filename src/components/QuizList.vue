907444dd4e7a57fc4a6f910338a452330a290c22
Jonas Lima <jonaslima015@gmail.com>
2020-09-23 16:07:41 -0300

    QuizList ajeitado

--- src/components/QuizList.vue
+++ src/components/QuizList.vue
@@ -3,11 +3,17 @@
     <div class="card bg-blue-container text-white mx-0">
       <div class="card-body">
         <h5 class="card-title">{{ quiz.title }}</h5>
-        <button class="btn btn-warning">
+        <button class="btn btn-primary mr-2">
+          <i class="fas fa-question" @click="editQuestions(quiz.id)"></i>
+        </button>
+        <button class="btn btn-warning mx-2">
           <i class="fas fa-pen" @click="editQuiz(quiz.id)"></i>
         </button>
         <button class="btn btn-success mx-2" @click="goToQuiz(quiz.id)">
           <i class="fas fa-play"></i>
+        </button>
+        <button class="btn btn-danger mx-2" @click="deleteQuiz(quiz.id)">
+          <i class="fas fa-trash"></i>
         </button>
       </div>
     </div>
@@ -18,11 +24,17 @@
 export default {
   props: ["quiz"],
   methods: {
+    editQuestions: function (id) {
+      this.$router.push({ name: "QuestionList", params: { id: id } });
+    },
     goToQuiz: function (id) {
       this.$router.push({ name: "Question", params: { id: id } });
     },
     editQuiz: function (id) {
-      this.$router.push({ name: "Quiz", params: { id: id } });
+      console.log(id); //A criar pagina para editar as informações do quiz
+    },
+    deleteQuiz: function (id) {
+      console.log(id); //Criar alert para confirmar
     },
   },
 };
