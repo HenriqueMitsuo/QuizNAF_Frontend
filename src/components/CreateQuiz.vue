50fc874596c5c3aa6b9a94285e89958e6ed9e405
Jonas Lima <jonaslima015@gmail.com>
2020-09-23 11:11:23 -0300

    adicionado formulário em CreateQuiz.vue

--- src/components/CreateQuiz.vue
+++ src/components/CreateQuiz.vue
@@ -1,5 +1,90 @@
 <template>
-  <div>
-    <h1>Criar Quiz</h1>
+  <div class="container">
+    <h2 class="text-white text-center mt-2">Criar Quiz</h2>
+    <form class="mt-4" @submit.prevent="createQuiz">
+      <!-- Titulo do quiz -->
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">
+            <i class="fas fa-font"></i>
+          </span>
+        </div>
+        <input
+          type="text"
+          v-model="QuizData.title"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Titulo"
+          required
+        />
+      </div>
+      <!-- Categoria do quiz -->
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" style="background: transparent;">
+            <i class="fas fa-align-left"></i>
+          </span>
+        </div>
+        <input
+          type="text"
+          v-model="QuizData.category"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Categoria"
+        />
+      </div>
+      <!-- Descrição do quiz  -->
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">
+            <i class="fas fa-align-right"></i>
+          </span>
+        </div>
+        <input
+          type="text"
+          v-model="QuizData.description"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Descrição"
+          required
+        />
+      </div>
+      <!-- Idioma do quiz -->
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" style="background: transparent;">
+            <i class="fas fa-language"></i>
+          </span>
+        </div>
+        <select class="form-control text-light" v-model="QuizData.lang">
+          <option value="pt-br">PT-BR</option>
+          <option value="es">ES</option>
+        </select>
+      </div>
+
+      <button type="submit" class="btn btn-success btn-block mt-3">Criar</button>
+    </form>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      QuizData: {
+        title: null,
+        category: null,
+        description: null,
+        lang: null,
+      },
+    };
+  },
+  methods: {
+    createQuiz: function () {
+      console.log(this.QuizData); //Log para testes
+    },
+  },
+};
+</script>
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="app">
+        <Sidebar />
         <CreateQuiz />
     </div> 
 </template>
 
 <script>
+import Sidebar from "@/components/Sidebar.vue";
 import CreateQuiz from "@/components/CreateQuiz.vue";
 export default {
     components: {
+        Sidebar,
         CreateQuiz,
     }
 }
