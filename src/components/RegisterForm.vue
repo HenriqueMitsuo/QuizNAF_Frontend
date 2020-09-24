d5933eb3f236c968d205347b0444fa2b9b9e29da
Jonas Lima <jonaslima015@gmail.com>
2020-09-24 11:02:39 -0300

    reposicionado opção de professor no registro

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -20,7 +20,7 @@
 
       <div class="input-group input-group-lg mb-3">
         <div class="input-group-prepend">
-          <span class="input-group-text" style="background: transparent;">
+          <span class="input-group-text" style="background: transparent">
             <i class="fas fa-envelope"></i>
           </span>
         </div>
@@ -54,7 +54,7 @@
 
       <div class="input-group input-group-lg mb-3">
         <div class="input-group-prepend">
-          <span class="input-group-text" style="background: transparent;">
+          <span class="input-group-text" style="background: transparent">
             <i class="fas fa-building"></i>
           </span>
         </div>
@@ -122,6 +122,18 @@
 
       <div class="input-group input-group-lg mb-3">
         <div class="input-group-prepend">
+          <span class="input-group-text" style="background: transparent">
+            <i class="fas fa-address-card"></i>
+          </span>
+        </div>
+        <select class="form-control text-light" v-model="User.role" required>
+          <option value="0" default>Sou aluno</option>
+          <option value="1">Sou professor</option>
+        </select>
+      </div>
+
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
           <span class="input-group-text">
             <i class="fas fa-lock"></i>
           </span>
@@ -151,18 +163,6 @@
           aria-describedby="inputGroup-sizing-sm"
           placeholder="Repita a senha"
         />
-      </div>
-
-      <div class="input-group input-group-lg mb-3">
-        <div class="input-group-prepend">
-          <span class="input-group-text" style="background: transparent;">
-            <i class="fas fa-address-card"></i>
-          </span>
-        </div>
-        <select class="form-control text-light" v-model="User.role" required>
-          <option value="0" default>Sou aluno</option>
-          <option value="1">Sou professor</option>
-        </select>
       </div>
 
       <button type="submit" class="btn btn-warning btn-block mt-3">REGISTRAR</button>
