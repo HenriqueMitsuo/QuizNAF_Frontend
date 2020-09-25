087096f1bf976ddf4b2d323dcba9ab274be73327
Jonas Lima <jonaslima015@gmail.com>
2020-09-25 10:28:00 -0300

    auto preenchimento dos dados no EditQuiz

--- src/components/EditQuiz.vue
+++ src/components/EditQuiz.vue
@@ -1,15 +1,100 @@
 <template>
-  <div>
-    <h1>Edit quiz</h1>
+  <div class="container">
+    <h3 class="text-white text-center mt-2">Atualizar dados do quiz</h3>
+    <form class="mt-4">
+      <!-- Titulo do quiz -->
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">
+            <i class="fas fa-font"></i>
+          </span>
+        </div>
+        <input
+          type="text"
+          v-model="QuizData.title"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Titulo"
+          required
+        />
+      </div>
+      <!-- Categoria do quiz -->
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" style="background: transparent">
+            <i class="fas fa-align-left"></i>
+          </span>
+        </div>
+        <input
+          type="text"
+          v-model="QuizData.category"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Categoria"
+          required
+        />
+      </div>
+      <!-- Descrição do quiz  -->
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">
+            <i class="fas fa-align-right"></i>
+          </span>
+        </div>
+        <input
+          type="text"
+          v-model="QuizData.description"
+          class="form-control text-light"
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+          placeholder="Descrição"
+          required
+        />
+      </div>
+      <!-- Idioma do quiz -->
+      <div class="input-group input-group-lg mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" style="background: transparent">
+            <i class="fas fa-language"></i>
+          </span>
+        </div>
+        <select class="form-control text-light" v-model="QuizData.lang" required>
+          <option value="pt-br">PT-BR</option>
+          <option value="es">ES</option>
+        </select>
+      </div>
+
+      <button type="submit" class="btn btn-success btn-block mt-3">Salvar alterações</button>
+      <router-link class="btn btn-danger btn-block mt-3" to="/Professor">Voltar</router-link>
+    </form>
   </div>
 </template>
 
 <script>
+import { ApiService as QuizService } from "@/services/ApiService";
+
 export default {
   data() {
     return {
+      quizService: new QuizService("quiz"),
       quiz_id: this.$route.params.id,
+      QuizData: {
+        title: null,
+        category: null,
+        description: null,
+        lang: null,
+      },
     };
+  },
+  async mounted() {
+    this.autoFill(this.quiz_id); //Auto preenche os dados
+  },
+  methods: {
+    autoFill: async function (id) {
+      this.QuizData = await this.quizService.queryOne(id);
+    },
   },
 };
 </script>
--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="app">
+    <Sidebar />
     <EditQuiz />
   </div>
 </template>
 
 <script>
+import Sidebar from "@/components/Sidebar.vue";
 import EditQuiz from "@/components/EditQuiz.vue";
 export default {
   components: {
+    Sidebar,
     EditQuiz,
   },
 };
