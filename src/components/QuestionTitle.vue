167629eacfbf4d8bb570ff6baf8aeb8544230f2a
Jonas Lima <jonaslima015@gmail.com>
2020-09-25 19:00:40 -0300

    modal para confirmar exclusão

--- src/components/QuestionTitle.vue
+++ src/components/QuestionTitle.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="col-12 my-3">
-    <div class="card bg-blue-container text-white mx-0">
-      <div class="card-body">
-        <h5 class="card-title">{{ question.title }}</h5>
-        <button class="btn btn-warning mx-2" @click="editQuestion(question.id)">
-          <i class="fas fa-pen"></i>
-        </button>
-        <button class="btn btn-danger mx-2" @click="deleteQuestion(question.id)">
-          <i class="fas fa-trash"></i>
-        </button>
+  <div>
+    <div class="col-12 my-3">
+      <div class="card bg-blue-container text-white mx-0">
+        <div class="card-body">
+          <h5 class="card-title">{{ question.title }}</h5>
+          <button class="btn btn-warning mx-2" @click="editQuestion(question.id)">
+            <i class="fas fa-pen"></i>
+          </button>
+          <button class="btn btn-danger mx-2" data-toggle="modal" data-target="#deleteThis">
+            <i class="fas fa-trash"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="deleteThis"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="deleteThisTitle"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="bg-app-primary text-center text-white modal-content">
+          <div class="modal-body">
+            <h5>Confimar ação?</h5>
+            Ao confirmar, não será possível reverter essa operação.
+          </div>
+          <div class="modal-body">
+            <button
+              type="button"
+              class="btn btn-success mx-2"
+              data-dismiss="modal"
+              @click="deleteQuestion(question.id)"
+            >
+              Apagar
+            </button>
+            <button type="button" class="btn btn-danger mx-2" data-dismiss="modal">Cancelar</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -18,11 +49,13 @@
 export default {
   props: ["question"],
   methods: {
+    //A criar pagina para editar as informações da questão
     editQuestion: function (id) {
-      console.log(id); //A criar pagina para editar as informações da questão
+      console.log(id);
     },
+    //Apagar quiz depois de confirmar
     deleteQuestion: function (id) {
-      console.log(id); //Criar alert para confirmar
+      console.log(id);
     },
   },
 };
