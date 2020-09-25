cdf120228ff44d99a0401cd51374083695b5f431
HenriqueMitsuo <henrique.informatica123@gmail.com>
2020-09-25 19:32:59 -0300

    Ajuste em QuestionTitle

--- src/components/QuestionTitle.vue
+++ src/components/QuestionTitle.vue
@@ -1,27 +1,27 @@
 <template>
-  <div>
-    <div class="col-12 my-3">
-      <div class="card bg-blue-container text-white mx-0">
-        <div class="card-body">
-          <h5 class="card-title">{{ question.title }}</h5>
-          <button class="btn btn-warning mx-2" @click="editQuestion(question.id)">
-            <i class="fas fa-pen"></i>
-          </button>
-          <button class="btn btn-danger mx-2" data-toggle="modal" data-target="#deleteThis">
-            <i class="fas fa-trash"></i>
-          </button>
-        </div>
+  <div class="col-12 my-3">
+    <div class="card bg-blue-container text-white mx-0">
+      <div class="card-body">
+        <h5 class="card-title">{{ question.title }}</h5>
+        <button class="btn btn-warning mx-2" @click="editQuestion(question.id)">
+          <i class="fas fa-pen"></i>
+        </button>
+        <button 
+          class="btn btn-danger mx-2" 
+          data-toggle="modal" 
+          :data-target="`#deleteThis${question.id}`">
+          <i class="fas fa-trash"></i>
+        </button>
       </div>
     </div>
     <!-- Modal -->
     <div
       class="modal fade"
-      id="deleteThis"
+      :id='`deleteThis${question.id}`'
       tabindex="-1"
       role="dialog"
       aria-labelledby="deleteThisTitle"
-      aria-hidden="true"
-    >
+      aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="bg-app-primary text-center text-white modal-content">
           <div class="modal-body">
@@ -33,8 +33,7 @@
               type="button"
               class="btn btn-success mx-2"
               data-dismiss="modal"
-              @click="deleteQuestion(question.id)"
-            >
+              @click="deleteQuestion(question.id)">
               Apagar
             </button>
             <button type="button" class="btn btn-danger mx-2" data-dismiss="modal">Cancelar</button>
@@ -47,7 +46,14 @@
 
 <script>
 export default {
-  props: ["question"],
+  props: {
+    question: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
   methods: {
     //A criar pagina para editar as informações da questão
     editQuestion: function (id) {
