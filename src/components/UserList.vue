c737cf480a2a38d563a3ac1b532fa1b9696a9f0a
Jonas Lima <jonaslima015@gmail.com>
2020-09-28 11:07:40 -0300

    adicionado UserList na aba admin

---
+++ src/components/UserList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col-12 my-3">
+    <div class="card bg-blue-container text-white mx-0">
+      <div class="card-body">
+        <h5 class="card-title">example@email.com</h5>
+        <p class="card-text">Usuário comum</p>
+        <button class="btn btn-success mx-2">
+          <i class="fas fa-arrow-up"></i>
+        </button>
+        <button class="btn btn-warning mx-2" disabled>
+          <i class="fas fa-arrow-down"></i>
+        </button>
+        <button class="btn btn-danger mx-2">
+          <i class="fas fa-trash"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,13 +1,22 @@
 <template>
-  <h1 class="text-light">Admin</h1>
+  <div class="app">
+    <Sidebar />
+    <div class="container text-light">
+      <h3 class="text-center my-2">Lista de usuários</h3>
+    </div>
+    <UserList />
+  </div>
 </template>
 
 <script>
+import Sidebar from "@/components/Sidebar";
+import UserList from "@/components/UserList";
+
 export default {
-    name: 'admin',
-}
+  components: {
+    Sidebar,
+    UserList,
+  },
+  name: "admin",
+};
 </script>
-
-<style>
-
-</style>
